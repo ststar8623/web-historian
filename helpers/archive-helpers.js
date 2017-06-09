24f60103db2417300c82708725c35f7125f60479
ststar8623 <ststar8623@gmail.com>
2017-06-09 16:41:21 -0700

    first commit

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,19 +1,26 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+// var figlet = require('figlet');
 
+// figlet.text('@ HACK REACTOR', function(error, data) {
+//   if (error) console.log(error);
+//   else console.log(data);
+// })
+/*
+    ____    _   _    _    ____ _  __  ____  _____    _    ____ _____ ___  ____  
+   / __ \  | | | |  / \  / ___| |/ / |  _ \| ____|  / \  / ___|_   _/ _ \|  _ \ 
+  / / _` | | |_| | / _ \| |   | ' /  | |_) |  _|   / _ \| |     | || | | | |_) |
+ | | (_| | |  _  |/ ___ \ |___| . \  |  _ <| |___ / ___ \ |___  | || |_| |  _ < 
+  \ \__,_| |_| |_/_/   \_\____|_|\_\ |_| \_\_____/_/   \_\____| |_| \___/|_| \_\
+   \____/                                                                       
+*/
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
  * if you move any files, you'll only need to change your code in one place! Feel free to
  * customize it in any way you wish.
  */
-
-exports.paths = {
-  siteAssets: path.join(__dirname, '../web/public'),
-  archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
-};
 
 // Used for stubbing paths for tests, do not modify
 exports.initialize = function(pathsObj) {
@@ -25,13 +32,32 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(callback) {
+exports.readListOfUrls = function(url, callback) {
+  let path = this.paths.siteAssets + url;
+  fs.readFile(path, 'utf8', function(err, data) {
+    // console.log('data: ', data);
+    if (err) {
+      return callback(err);
+    } else {
+      return callback(data);
+    }
+  });
+};
+
+exports.paths = {
+  siteAssets: path.join(__dirname, '../web/public'),
+  archivedSites: path.join(__dirname, '../archives/sites'),
+  list: path.join(__dirname, '../archives/sites.txt')
 };
 
 exports.isUrlInList = function(url, callback) {
+  fs.readFile(this.paths.list, 'utf8', function(err, data) {
+    console.log(data);
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+
 };
 
 exports.isUrlArchived = function(url, callback) {
--- web/request-handler.js
+++ web/request-handler.js
@@ -3,5 +3,37 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  let statusCode;
+
+  headers['Content-Type'] = 'text/html';
+
+  if (req.method === 'GET') {
+    statusCode = 200;
+    res.writeHead(statusCode, headers);
+    archive.readListOfUrls('/index.html', function(data) {
+      // console.log('req Data: ', data);
+      res.end(data);
+    });
+
+    // res.write(archive.paths.siteAssets + '/index.html');
+  }
+  // if (req.method === 'POST') {
+  //   statusCode = 201;
+  //   res.writeHead(statusCode, headers);
+  //   req.on('data', function(chunk) {
+  //     console.log('Got to POST');
+  //     res.write(chunk.toString());
+  //   });
+  //   req.on('end', function() {
+  //     res.end();
+  //   })
+  // }
+  // res.end(archive.paths.list);
+};
+
+let defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, text/html',
+  'access-control-max-age': 10
 };
